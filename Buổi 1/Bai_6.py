c869438f1988bfd295408a8297d4aeadcfd33f9b
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-03 13:29:04 +0700

    Add files via upload

---
+++ Buổi 1/Bai_6.py
@@ -0,0 +1,3 @@
+do_c=float(input('Nhập độ C chuyển đổi: '))
+do_f= 9/6 * do_c + 32
+print('{:.1f} độ C = {:.1f} độ F'.format(do_c,do_f))
---
+++ Buổi 1/Bai_8.py
@@ -0,0 +1,4 @@
+chuoi_ki_tu=input('Nhập 1 chuỗi kí tự bất kì để xử lý: ')
+print('Số kí tự của chuỗi: ', len(chuoi_ki_tu))
+print('In 4 kí tự đầu tiên: ', chuoi_ki_tu[:4])
+print('In 4 kí tự cuối cùng: ', chuoi_ki_tu[-4:])

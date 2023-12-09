1671e6513db5c0c49ce96c725263fc0463ce98e1
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-10 02:14:08 +0700

    Delete Buổi 1 directory

--- Buổi 1/Bai_6.py
+++
@@ -1,3 +0,0 @@
-do_c=float(input('Nhập độ C chuyển đổi: '))
-do_f= 9/6 * do_c + 32
-print('{:.1f} độ C = {:.1f} độ F'.format(do_c,do_f))
--- Buổi 1/Bai_8.py
+++
@@ -1,7 +0,0 @@
-chuoi_ki_tu=input('Nhập 1 chuỗi kí tự bất kì để xử lý: ')
-print('Số kí tự của chuỗi: ', len(chuoi_ki_tu))
-print('In 4 kí tự đầu tiên: ', chuoi_ki_tu[:4])
-print('In 4 kí tự cuối cùng: ', chuoi_ki_tu[-4])
-
-#optional
-print('In ký tự từ 0->6 cách ra 2 step (xét index)', chuoi_ki_tu[0:7:2])

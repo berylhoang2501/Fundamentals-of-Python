cea2fcf7bb03b57ac2b1e599ec325e99eee95e07
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-10 02:09:49 +0700

    Delete Buổi 1 directory

--- Buổi 1/Bai_7.py
+++
@@ -1,8 +0,0 @@
-# Lệnh nhập lần lượt
-hana,tul,set=eval(input('Nhập vào lần lượt số kẹo của 3 đứa trẻ: '))
-
-#Chia lấy phần dư
-print('Số kẹo cần huỷ là: ',(hana+tul+set)%3)
-
-#chia lấy phần nguyên
-print('Số kẹo mỗi đứa trẻ nhận được: {} viên kẹo'.format((hana+tul+set)//3))

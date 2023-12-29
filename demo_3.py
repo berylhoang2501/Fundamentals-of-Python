baa4d41b0a99ad1a0c26a6da73e0da555fb5935c
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-30 01:13:22 +0700

    Delete demo_3.py

--- demo_3.py
+++
@@ -1,9 +0,0 @@
-print('Các số chẵn là: ')
-
-for i in range(0,11,2):
-    print(i,end=',')
-    
-# đối với trường hợp không biết step = bao nhiêu
-# for i in range(0,11):
-#     if i%2==0:
-#         print(i,end=',')

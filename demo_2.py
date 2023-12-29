ed19a501cda33f75eef476108ee662b7a9dac839
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-30 01:13:13 +0700

    Delete demo_2.py

--- demo_2.py
+++
@@ -1,9 +0,0 @@
-#Cấu trúc lặp for trong trường hợp lặp với số lần biết trước
-while True:
-    n=int(input('Nhập n để countdown: '))
-    if n>0:
-        break
-    else: 
-        print('Số countdown cần >0')
-for i in range(n,0,-1): #start_stop_step
-    print(i,end=',')

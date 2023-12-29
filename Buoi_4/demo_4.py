10c335e830ae0c63970933fb48ef43f0eb905d0b
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-30 01:24:26 +0700

    Delete Buoi_4 directory

--- Buoi_4/demo_4.py
+++
@@ -1,9 +0,0 @@
-# str_season='String Summer Fall Winter'
-# list_season=str_season.split()
-# print('List season: ',list_season)
-# print('First season: ',list_season[0])
-# print('Last seasin: ',list_season[-1])
-
-list_family=['Chau', 'Thai','Hai','Quang' ]
-print('My family: ','-'.join(list_family))
-

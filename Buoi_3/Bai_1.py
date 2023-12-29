71a79f7af82129b3854669aa13d32acd3ec54a87
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2023-12-30 00:52:40 +0700

    Delete Buoi_3 directory

--- Buoi_3/Bai_1.py
+++
@@ -1,9 +0,0 @@
-#Viết chương trình countdown từ n về 1 (n>0) theo 2 cách.
-
-n=int(input('Nhập n để countdown: '))
-
-while n>0:
-    print(n)
-    n=n-1
-    
- 
--- Buoi_3/demo_4.py
+++
@@ -1,3 +0,0 @@
-cau=input('Nhập 1 câu: ')
-for ki_tu in cau:
-    print(ki_tu, end=',')

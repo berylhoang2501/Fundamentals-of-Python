0bf2526d56b8f1d9f2b6012d5567889a0d4a3e63
berylhoang2501 <152646327+berylhoang2501@users.noreply.github.com>
2024-04-06 13:47:53 +0700

    Delete Hoàng Ngọc Thuỷ Thương_DL01 directory

--- Hoàng Ngọc Thuỷ Thương_DL01/Baitap_2/Ve_hinh_chu_nhat.py
+++
@@ -1,9 +0,0 @@
-while True:
-    dai = int(input("Nhập chiều dài của hình chữ nhật: "))
-    rong = int(input("Nhập chiều rộng của hình chữ nhật: "))
-    ky_tu = input("Nhập ký tự để vẽ hình chữ nhật: ")
-
-    for i in range(rong):
-        for j in range(dai):
-            print(ky_tu, end=' ')
-        print()
